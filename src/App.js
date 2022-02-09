920f0f982e951bcdee142d0bfac7e5d23188a6c3
Kumail Khan <hassankumail9@gmail.com>
2022-02-09 16:44:12 +0500

    added color changes, size changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,95 @@
-import Button from "./Components/Button";
+/* eslint-disable no-useless-escape */
+import Button from "./Components/Button/Button";
 import classes from "./App.module.css";
 function App() {
   return (
-    <div className={classes.App}>
-      <Button>First Button</Button>
-      <Button variant='outline'>Outline</Button>
-      <Button variant='text'>Text</Button>
-      <Button disableShadow>Disabled Shadow</Button>
-      <Button disabled>Disabled</Button>
-      <Button variant ='text' disabled>Text Disabled</Button>
+    <div>
+      <h1>Buttons</h1>
+
+      <div className={classes.App}>
+        <p>{`<Button \>`}</p>
+        <p>{`&:hover, &:focus`}</p>
+      </div>
+
+      <div className={classes.App}>
+        <Button>Default</Button>
+        <Button>Default</Button>
+      </div>
+
+      <div className={classes.App}>
+        <p>{`<Button variant='outline'\>`}</p>
+        <p>{`&:hover, &:focus`}</p>
+      </div>
+
+      <div className={classes.App}>
+        <Button variant='outline'>Default</Button>
+        <Button variant='outline'>Default</Button>
+      </div>
+
+      <div className={classes.App}>
+        <p>{`<Button variant='text'\>`}</p>
+        <p>{`&:hover, &:focus`}</p>
+      </div>
+
+      <div className={classes.App}>
+        <Button variant='text'>Default</Button>
+        <Button variant='text'>Default</Button>
+      </div>
+
+      <div className={classes.App}>
+        <p>{`<Button disableShadow />`}</p>
+      </div>
+
+      <div className={classes.App}>
+        <Button disableShadow>Default</Button>
+      </div>
+
+      <div className={classes.App}>
+        <p>{`<Button disabled />`}</p>
+        <p>{`<Button variant='text' disabled>`}</p>
+      </div>
+
+      <div className={classes.App}>
+        <Button disabled>Default</Button>
+        <Button variant='text' disabled>Text Disabled</Button>
+      </div>
+
+      <div className={classes.App}>
+        <p>{`<Button startIcon='local_grocery_store'>`}</p>
+        <p>{`<Button endIcon='local_grocery_store'>`}</p>
+      </div>
+
+      <div className={classes.App}>
+        <Button startIcon='local_grocery_store'>Start Icon</Button>
+        <Button endIcon='local_grocery_store'>End Icon</Button>
+      </div>
+
+      <div className={classes.App}>
+        <p>{`<Button size='sm'>`}</p>
+        <p>{`<Button size='md'>`}</p>
+        <p>{`<Button size='lg'>`}</p>
+      </div>
+
+      <div className={classes.App}>
+        <Button size='sm'>Small</Button>
+        <Button size='md'>Medium</Button>
+        <Button size='lg'>Large</Button>
+      </div>
+
+      <div className={classes.App}>
+        <p>{`<Button color='default'>`}</p>
+        <p>{` <Button color='primary'>`}</p>
+        <p>{` <Button color='secondary'>`}</p>
+        <p>{` <Button color='danger'>`}</p>
+      </div>
+
+      <div className={classes.App}>
+        <Button color='default'>Default</Button>
+        <Button color='primary'>Default</Button>
+        <Button color='secondary'>Default</Button>
+        <Button color='danger'>Default</Button>
+
+      </div>
     </div>
   );
 }
---
+++ src/Components/Button/Button.js
@@ -0,0 +1,55 @@
+
+import classes from './Button.module.css';
+import { MdOutlineLocalGroceryStore } from 'react-icons/md';
+const Button = (
+    {
+        children,
+        variant,
+        disableShadow,
+        disabled,
+        startIcon,
+        endIcon,
+        size,
+        color
+    }
+) => (
+    <button className=
+        {`
+            ${classes.Button} 
+            ${variant === 'outline' && classes.Outline}
+            ${variant === 'text' && classes.Text}
+            ${disableShadow && classes.DisableShadow}
+            ${disabled && classes.TextDisabled}
+            ${(startIcon || endIcon) && classes.Icon}
+            ${size === 'sm' && classes.Small}
+            ${size === 'md' && classes.Medium}
+            ${size === 'lg' && classes.Large}
+            ${color === 'default' ? classes.Default :
+                color === 'primary' ? classes.Primary :
+                    color === 'secondary' ? classes.Secondary :
+                        color === 'danger' ? classes.Danger :
+                            color === '' ? classes.Default : ''
+            }
+          
+        `}
+        disabled={disabled}
+
+    >
+        <span>
+            {
+
+                startIcon === 'local_grocery_store' ? <MdOutlineLocalGroceryStore color='white' /> : ''
+            }
+        </span>
+        {children}
+        <span>
+            {
+
+                endIcon === 'local_grocery_store' ? <MdOutlineLocalGroceryStore color='white' /> : ''
+            }
+        </span>
+
+    </button>
+);
+
+export default Button;
